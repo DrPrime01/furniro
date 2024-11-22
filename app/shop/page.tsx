ccdf373917a7d6c3e09de32731d66ff8446e99d1
DrPrime01 <drprime.dev@gmail.com>
2024-11-22 12:29:06 +0100

    feat: implemented banner component

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,6 +1,7 @@
+import Banner from "@/components/Banner";
 import Pagination from "@/components/Pagination";
 import ProductList from "@/components/ProductList";
-import { toCurrency } from "@/utils/helpers";
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -9,6 +10,7 @@ function page() {
   return (
     <div className="flex flex-col">
       <div className="h-[316px] relative bg-[url('/images/shop-hero.png')] bg-no-repeat flex flex-col items-center justify-center gap-0.5">
+        <Image src="/icons/logo.svg" alt="logo" width={77} height={77} />
         <h2 className="font-medium text-5xl leading-[4rem] text-black">Shop</h2>
         <div className="flex items-center gap-x-1.5">
           <Link href="/" className="text-black font-medium">
@@ -71,63 +73,7 @@ function page() {
           <Pagination />
         </div>
       </div>
-      <div className="py-[100px] px-14 flex flex-wrap justify-center lg:grid lg:grid-cols-4 max-lg:gap-14 bg-cream-3">
-        <div className="flex gap-2.5">
-          <Image src="/icons/trophy.svg" alt="trophy" width={60} height={60} />
-          <div className="flex flex-col gap-0.5">
-            <h3 className="font-semibold text-xl text-gray-dark">
-              High Quality
-            </h3>
-            <p className="text-gray-2 font-medium text-lg">
-              Crafted from top materials
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-2.5">
-          <Image
-            src="/icons/guarantee.svg"
-            alt="guarantee"
-            width={60}
-            height={60}
-          />
-          <div className="flex flex-col gap-0.5">
-            <h3 className="font-semibold text-xl text-gray-dark">
-              Warranty Protection
-            </h3>
-            <p className="text-gray-2 font-medium text-lg">Over 2 years</p>
-          </div>
-        </div>
-        <div className="flex gap-2.5">
-          <Image
-            src="/icons/shipping.svg"
-            alt="shipping"
-            width={60}
-            height={60}
-          />
-          <div className="flex flex-col gap-0.5">
-            <h3 className="font-semibold text-xl text-gray-dark">
-              Free Shipping
-            </h3>
-            <p className="text-gray-2 font-medium text-lg">
-              For orders over {toCurrency(150000)}
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-2.5">
-          <Image
-            src="/icons/customer-support.svg"
-            alt="customer-support"
-            width={60}
-            height={60}
-          />
-          <div className="flex flex-col gap-0.5">
-            <h3 className="font-semibold text-xl text-gray-dark">
-              24/7 Support
-            </h3>
-            <p className="text-gray-2 font-medium text-lg">Dedicated support</p>
-          </div>
-        </div>
-      </div>
+      <Banner />
     </div>
   );
 }
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { toCurrency } from "@/utils/helpers";
+import Image from "next/image";
 
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div className="py-[100px] px-14 flex flex-wrap justify-center lg:grid lg:grid-cols-4 max-lg:gap-14 bg-cream-3">
+      <div className="flex gap-2.5">
+        <Image src="/icons/trophy.svg" alt="trophy" width={60} height={60} />
+        <div className="flex flex-col gap-0.5">
+          <h3 className="font-semibold text-xl text-gray-dark">High Quality</h3>
+          <p className="text-gray-2 font-medium text-lg">
+            Crafted from top materials
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-2.5">
+        <Image
+          src="/icons/guarantee.svg"
+          alt="guarantee"
+          width={60}
+          height={60}
+        />
+        <div className="flex flex-col gap-0.5">
+          <h3 className="font-semibold text-xl text-gray-dark">
+            Warranty Protection
+          </h3>
+          <p className="text-gray-2 font-medium text-lg">Over 2 years</p>
+        </div>
+      </div>
+      <div className="flex gap-2.5">
+        <Image
+          src="/icons/shipping.svg"
+          alt="shipping"
+          width={60}
+          height={60}
+        />
+        <div className="flex flex-col gap-0.5">
+          <h3 className="font-semibold text-xl text-gray-dark">
+            Free Shipping
+          </h3>
+          <p className="text-gray-2 font-medium text-lg">
+            For orders over {toCurrency(150000)}
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-2.5">
+        <Image
+          src="/icons/customer-support.svg"
+          alt="customer-support"
+          width={60}
+          height={60}
+        />
+        <div className="flex flex-col gap-0.5">
+          <h3 className="font-semibold text-xl text-gray-dark">24/7 Support</h3>
+          <p className="text-gray-2 font-medium text-lg">Dedicated support</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
