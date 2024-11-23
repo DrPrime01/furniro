93cb20149b446ee368427a74a58da186b5d4b5da
DrPrime01 <drprime.dev@gmail.com>
2024-11-23 10:35:53 +0100

    feat: added my wishlist page

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Banner from "@/components/Banner";
+import HeroBanner from "@/components/HeroBanner";
+import Pagination from "@/components/Pagination";
+import ProductList from "@/components/ProductList";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col">
+      <HeroBanner name="My Wishlist" />
+      <div className="container py-12 flex flex-col gap-y-16">
+        <ProductList />
+        <div className="self-center">
+          <Pagination />
+        </div>
+      </div>
+      <Banner />
+    </div>
+  );
 }
 
 export default page;
