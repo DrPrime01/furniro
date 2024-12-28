edd13246c62b4906aab04d2cc0dfc87efb323a06
DrPrime01 <drprime.dev@gmail.com>
2024-12-28 12:03:02 +0100

    feat: commented products list on wishlist

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,14 +1,14 @@
 import Banner from "@/components/Banner";
 import HeroBanner from "@/components/HeroBanner";
 import Pagination from "@/components/Pagination";
-import ProductList from "@/components/ProductList";
+// import ProductList from "@/components/ProductList";
 
 function page() {
   return (
     <div className="flex flex-col">
       <HeroBanner name="My Wishlist" />
       <div className="container py-12 flex flex-col gap-y-16">
-        <ProductList />
+        {/* <ProductList /> */}
         <div className="self-center">
           <Pagination />
         </div>
--- todo.md
+++ todo.md
@@ -3,3 +3,6 @@
 - When a user clicks the like button, the product should be added to the wishlist (requires auth)
 - When a user clicks the share button, a modal should pop-up to show which social media the user wants to share the product to.
 - Implement search UI. When you click the search icon in the navbar, a search component drops down from the top to allow you to enter the search keyword to look for.
+- Implement cart
+- Implement auth
+- Implement blog
